19e41a6384d90eb6321d6b4f814bb443972f4091
Merak Bourque <xtremeyou@hotmail.com>
2024-07-20 15:18:33 -0400

    refactored spinner

--- spinner2.js
+++ spinner2.js
@@ -1,12 +1,10 @@
-process.stdout.write('hello from spinner1.js... \rheyyy\n');
-
-
-const sentence = "\r|  \r/  \r-  \r\  \r|  \r/  \r-  \r\  \r|  \n";
+const sentence = ["\r|  ", "\r/  ",  "\r-  ", "\r\\  ", "\r|  ", "\r/  ", "\r-  ", "\r\\  ", "\r|  ", "\n"];
 
 setTimeout(() => {
-  for (let i = 0; i < sentence.length; i++) {
-    setTimeout(() => {
-      process.stdout.write(sentence[i]);
-    }, 200 * i);
-  }
-});
+    for (let i = 0; i < sentence.length; i++) {
+      setTimeout(() => {
+        process.stdout.write(sentence[i]);
+      }, 200 * i);
+    }
+  });
+  
